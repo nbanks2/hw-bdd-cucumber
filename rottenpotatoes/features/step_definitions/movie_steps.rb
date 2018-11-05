490f3101e7d9e7b17d91b61a07a5c82bb56fdcf1
Janet Burge <banks.nadia@gmail.com>
2018-11-05 14:48:02 -0700

    I can see e1 before e2 passing

--- rottenpotatoes/features/step_definitions/movie_steps.rb
+++ rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -17,9 +17,11 @@ end
 #   on the same page
 
 Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
+  #  ensure that that e1 occurs before e2.  
+      (page.body.index(e1) < page.body.index(e2)).should == true
+  
   #  page.body is the entire content of the page as a string.
-  fail "Unimplemented"
+ # fail "Unimplemented"
 end
 
 # Make it easier to express checking or unchecking several boxes at once
